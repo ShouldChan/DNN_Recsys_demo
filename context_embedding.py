eeba47ad27ebb38fcb7b7a31193955787fcec166
ShouldChan <ezanmydream@163.com>
2017-08-14 16:18:30 +0800

    embedding_0418

--- context_embedding.py
+++ context_embedding.py
@@ -5,36 +5,44 @@ import time
 import math
 
 data_dir = './hetrec2011-lastfm-2k/'
+model_dir = './context_model/'
 
 # Parameters
-context_weight
-lamda
-gamma
-dis_coef
-alpha
+lamda = 0.03
+learning_rate = 0.005
+dis_coef = 0.25
+alpha = 0.2
+max_iters = 50
 
 def read_dataset():
-    # step1----------read dataset
-    header = ['userID', 'artistID', 'weight']
-    df = pd.read_csv(data_dir + 'user_artists.dat', sep = '\t', names = header)
+    train_data = []
+    user_set = set()
+    user_list = []
+    artist_set = set()
+    artist_list = []
 
-    users = df.userID.unique()
-    items = df.artistID.unique()
+    with open(data_dir + 'user_artists_edit.dat', 'rb') as fp:
+        lines = fp.readlines()
+        for line  in lines:
+            tempdata = line.strip().split('\t')
+            userid, artistid, weight = int(tempdata[0]), int(tempdata[1]), int(tempdata[2])
+            train_data.append([userid, artistid, weight])
+            user_set.add(userid)
+            artist_set.add(artistid)
+        
+        user_list = list(user_set)
+        user_list.sort()
+        print user_list
+        artist_list = list(artist_set)
+        artist_list.sort()
+        print artist_list
 
-    n_users = users.shape[0]
-    n_items = items.shape[0]
+    return train_data, user_list, artist_list
 
-    print type(users)
-    print users
-    print items
-    print df.head(3)
-    print df.describe()
-    print n_users, n_items
-    return df, users, items, n_users, n_items
+# read_dataset()
 
-def read_context():
-    header = ['userID', 'artistID']
-    df = pd.read_csv(data_dir + 'user_friends.dat', seq = '\t', names = header)
+
+            
 
 def sigmoid(x):
     return 1.0 / (1 + math.exp(-x))
@@ -47,21 +55,26 @@ def embedding_learning(train_data):
 
     log_likelihood = 0.0
 
-    for iteration in range(max_iters):
-        t = time.time()
-        for each_data in train_data:
-            u_i, i, w_i = each_data
-            for u_j in context_list[u_i]:
-                Di = np.linalg.norm(IC[i] - UC[u_i]) ** 2
-                Dj = np.linalg.norm(IC[i] - UC[u_j]) ** 2
+    try:
+        for iteration in range(max_iters):
+            t = time.time()
+            for each_data in train_data:
+                u_i, i, w_i = each_data
+                for u_j in context_list[u_i]:
+                    Di = np.linalg.norm(IC[i] - UC[u_i]) ** 2
+                    Dj = np.linalg.norm(IC[i] - UC[u_j]) ** 2
 
-                z = Dj - Di
+                    z = Dj - Di
 
-                wc = w_i ** dis_coef
-                
-                log_likelihood += np.log(sigmoid(z))
+                    wc = w_i ** dis_coef
+                    
+                    log_likelihood += np.log(sigmoid(z))
 
-                IC[i] += gamma * ((1 - sigmoid(z)) * 2 * alpha * wc * (UC[u_i] - UC[u_j]) - 2 * lamda * IC[i])
-                UC[u_i] += gamma * ((1 - sigmoid(z)) * 2 * alpha * wc * (IC[i] - UC[u_i]) - 2 * lamda * UC[u_i])
-                UC[u_j] += gamma * ((1 - sigmoid(z)) * 2 * alpha * wc * (IC[i] - UC[u_j]) - 2 * lamda * UC[u_j])
-        print 'Iter: %d    likelihood: %f   elapsed:  %fseconds'%(iteration, log_likelihood, time.time() - t)
+                    IC[i] += learning_rate * ((1 - sigmoid(z)) * 2 * alpha * wc * (UC[u_i] - UC[u_j]) - 2 * lamda * IC[i])
+                    UC[u_i] += learning_rate * ((1 - sigmoid(z)) * 2 * alpha * wc * (IC[i] - UC[u_i]) - 2 * lamda * UC[u_i])
+                    UC[u_j] += learning_rate * ((1 - sigmoid(z)) * 2 * alpha * wc * (IC[i] - UC[u_j]) - 2 * lamda * UC[u_j])
+            print 'Iter: %d    likelihood: %f   elapsed:  %fseconds'%(iteration, log_likelihood, time.time() - t)
+    finally:
+        np.save(model_dir + 'IC', IC)
+        np.save(model_dir + 'UC', UC)
+        print 'Model saved...'
