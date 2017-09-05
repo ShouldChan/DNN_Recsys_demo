4b66ec00624adcf559992d4004022548b1ed6342
ShouldChan <ezanmydream@163.com>
2017-09-05 14:26:20 +0800

    embedding_0226

--- context_embedding.py
+++ context_embedding.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import math
 import random
+import keras
 from keras.layers import Embedding, Merge, LSTM, Input
 from keras import initializations
 from keras.regularizers import l1, l2, l1l2
@@ -201,30 +202,44 @@ if __name__ == '__main__':
     # model.fit(data, labels, nb_epoch=10, batch_size=32)
 
     # test_merge
-    left_branch = Sequential()
-    left_branch.add(Embedding(1000, 32, input_length=None))
-    left_branch.add(Dense(32, input_dim=100))
+    # left_branch = Sequential()
+    # left_branch.add(Embedding(1000, 32, input_length=None))
+    # left_branch.add(Dense(32, input_dim=100))
 
-    right_branch = Sequential()
-    right_branch.add(Embedding(1000, 32, input_length=None))
-    right_branch.add(Dense(32, input_dim=100))
+    # right_branch = Sequential()
+    # right_branch.add(Embedding(1000, 32, input_length=None))
+    # right_branch.add(Dense(32, input_dim=100))
 
-    merged = Merge([left_branch, right_branch], mode='concat')
+    # merged = Merge([left_branch, right_branch], mode='concat')
 
-    final_model = Sequential()
-    final_model.add(merged)
-    final_model.add(Dense(10, activation='softmax'))
+    # final_model = Sequential()
+    # final_model.add(merged)
+    # final_model.add(Dense(10, activation='softmax'))
 
-    final_model.compile(optimizer='rmsprop', \
-            loss='binary_crossentropy', \
-            metrics=['accuracy'])
+    # final_model.compile(optimizer='rmsprop', \
+    #         loss='binary_crossentropy', \
+    #         metrics=['accuracy'])
 
-    data_1 = np.random.random((1000, 100))
-    data_2 = np.random.random((1000, 100))
+    # data_1 = np.random.random((1000, 100))
+    # data_2 = np.random.random((1000, 100))
 
-    from keras.utils.np_utils import to_categorical
-    labels = np.random.randint(10, size=(1000, 1))
-    labels = to_categorical(labels, 10)
+    # from keras.utils.np_utils import to_categorical
+    # labels = np.random.randint(10, size=(1000, 1))
+    # labels = to_categorical(labels, 10)
 
     
-    final_model.fit([data_1, data_2], labels, nb_epoch=10, batch_size=32)
+    # final_model.fit([data_1, data_2], labels, nb_epoch=10, batch_size=32)
+
+    # test_5
+    main_input = Input(shape=(100,), dtype='int32', name='main_input')
+    x = Embedding(output_dim=512, input_dim=10000, input_length=100)(main_input)
+    lstm_out = LSTM(32)(x)
+    auxiliary_output = Dense(1, activation='sigmoid', name='aux_output')(lstm_out)
+
+    auxiliary_input = Input(shape=(5,), name='aux_input')
+    x = Merge([lstm_out, auxiliary_input], mode='concat')
+    x = Dense(64, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+
+    main_output = Dense(1, activation='sigmoid', name='main_output')(x)
